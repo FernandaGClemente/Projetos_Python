1c7749494a4d7e8bb487948bacb3013dda39936a
FernandaGClemente <107092677+FernandaGClemente@users.noreply.github.com>
2023-03-06 17:17:22 -0300

    Update __init__.py

--- Site_ComunidadeDataAnalyst/ComunidadeDataAnalyst/__init__.py
+++ Site_ComunidadeDataAnalyst/ComunidadeDataAnalyst/__init__.py
@@ -2,12 +2,17 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+import os
 
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '651bb75489ae2cc3cbc5668710646384'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ComunidadeDataAnalyst.db'
+
+if os.getenv("DATABASE_URL"):
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ComunidadeDataAnalyst.db'
 
 database = SQLAlchemy(app)
 
